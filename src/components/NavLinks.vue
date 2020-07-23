d55d7d7ce60260303f0440f91180c0aa3cd0e122
Lisa S <lxndrmnt@gmail.com>
2020-07-23 13:49:59 +0300

    component: NavLinks - display of active service

--- src/components/NavLinks.vue
+++ src/components/NavLinks.vue
@@ -1,13 +1,13 @@
 <template>
   <div :id="name" class="w3-hide w3-white w3-card-4">
     <router-link
-      class="item w3-bar-item w3-button w3-border-bottom"
       v-for="service in services"
       :key="service.url"
       :to="'/' + service.familyName + '/' + service.url"
+      class="item w3-bar-item w3-button w3-border-bottom"
     >
       <div>
-        {{ service.title }}
+        <span :class="{ active: service.url == url }">{{ service.title }}</span>
         <span
           class="w3-tag w3-small"
           :class="{
@@ -29,6 +29,12 @@ export default {
   props: {
     name: String,
     services: Array
+  },
+  computed: {
+    url() {
+      if (this.$route.params) return this.$route.params.id
+      return null
+    }
   }
 }
 </script>
@@ -36,5 +42,8 @@ export default {
 <style scoped>
 .w3-card-4 {
   box-shadow: none !important;
+}
+.active {
+  font-weight: bold;
 }
 </style>
