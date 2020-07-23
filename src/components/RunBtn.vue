a9dd6d1252977ac256bb2c553ef780358263b4c9
Lisa S <lxndrmnt@gmail.com>
2020-07-23 16:14:14 +0300

    component: RunBtn - spinner align

--- src/components/RunBtn.vue
+++ src/components/RunBtn.vue
@@ -9,7 +9,7 @@
     </button>
     <i
       class="fa fa-spinner w3-spin w3-center"
-      style="font-size:20px; margin-left: 10px; vertical-align: center;"
+      style="font-size:20px; margin-left: 10px; vertical-align: middle;"
       v-if="isRunning"
     ></i>
   </p>
